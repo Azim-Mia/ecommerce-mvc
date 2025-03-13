7a863a4a72b510ca58d750e965b07b1d70f0a7e7
azim-mia <mdazimmia01980@gmail.com>
2025-03-13 11:50:16 +0600

    add code

---
+++ servises/clearCard.ts
@@ -0,0 +1,37 @@
+import redis from '../config/rdConfig';
+import axios from 'axios';
+//use session id
+const clearCard =async(id:string)=>{
+  //get valid card 
+const data = await redis.hgetall(`card:${id}`);
+//check this value
+if(Object.keys(data).length === 0){
+  return;
+};
+//create array 
+const items = Object.keys(data).map((key)=>{
+  const {inventoryId, quantity} = JSON.parse(data[key]) as {
+    inventoryId:string;
+    quantity:number;
+  };
+  return {
+    inventoryId, 
+    quantity,
+    productId:key
+  }
+});
+console.log("clearCard Items", items )
+// product inventory 
+const request = items.map(async(item)=>{
+  console.log("update Id:", item.inventoryId)
+  return await axios.put(`http://localhost:3001/inventoris/update/${item.inventoryId}`,{
+    quantity:Number(item.quantity),
+    actionType:"IN"
+  });
+});
+Promise.all(request);
+console.log("inventoryId update");
+//clear the card 
+await redis.del(`card:${id}`);
+}
+export default clearCard;
