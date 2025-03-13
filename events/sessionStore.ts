a2213a5b8aa4e8fddfb1041c3e1044fc76f387f1
azim-mia <mdazimmia01980@gmail.com>
2025-03-13 11:48:39 +0600

    add code

---
+++ events/sessionStore.ts
@@ -0,0 +1,21 @@
+import { Redis } from 'ioredis';
+import clearCard from "/data/data/com.termux/files/home/e-commerce-mvc/servises/clearCard"
+const redis = new Redis({
+  port: 10971,
+  host:'redis-10971.c9.us-east-1-4.ec2.redns.redis-cloud.com',
+  username: "default",
+  password: '4nkFu4I9c0MDBGHdhsaPkHYGXVeNyp6t',
+  db: 0, // Defaults to 0
+});
+const CHENAL_KEY ='__keyevent@0__:expired';
+redis.config('SET', 'notify-keyspace-events','Ex');
+redis.subscribe(CHENAL_KEY);
+redis.on('message', async(ch, message)=>{
+  if(ch == CHENAL_KEY){
+    console.log('expired key : ' + message);
+  const cardKey = message.split(":").pop();
+  if(!cardKey) return;
+  clearCard(cardKey);
+  }
+})
+/*this file use root file app */
