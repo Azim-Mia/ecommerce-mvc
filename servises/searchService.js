8d8a2729fdf9c2420d96faf7a7bba9b6245f8df1
azim-mia <mdazimmia01980@gmail.com>
2025-04-12 20:23:01 +0600

    add code

---
+++ servises/searchService.js
@@ -0,0 +1,38 @@
+//const mongoose=require('mongoose');
+//const createError= require('http-errors');
+const searchService=async(model,req)=>{
+  try{
+const text=req.query.name || "";
+   const page=Number(req.query.page) || 1;
+   const limit=Number(req.query.limit) || 5;
+   
+    const searchRegExp=new RegExp('.*' + text + '.*', 'i');
+    const filter={
+      isAdmin:{$ne:true},
+     $or:[
+  {name:{$regex:searchRegExp}},
+   {email:{$regex:searchRegExp}},
+  {phone:{$regex:searchRegExp}},
+        ],
+    };
+  const options= {password:0, _id:0,email:0};
+  const exists=await model.find(filter,options)
+  .limit(limit)
+  .skip((page-1) * limit);
+  if(!exists) throw createError(404,"not found users");
+  const count= await model.find(filter).countDocuments();
+ return {
+ payload:exists,
+ pagination:{
+    totalPage:Math.ceil(count/limit),
+    correntPage:page,
+    previousePage:page-1>0 ? page-1:null,
+    nextPage:page+1 < Math.ceil(count/limit) ? page + 1:null,
+  totalCount:count
+   },
+ }
+}catch(error){
+  return error;
+}
+}
+export default searchService;
